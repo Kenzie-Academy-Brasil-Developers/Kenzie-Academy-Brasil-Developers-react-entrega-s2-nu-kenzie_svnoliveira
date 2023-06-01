eea15ede50446da7905574b93987f48f1529502e
svnoliveira <srkuran@gmail.com>
2023-06-01 20:48:12 -0300

    parte logica iniciada

--- src/components/TotalMoney/style.js
+++ src/components/TotalMoney/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledSection = styled.section`
-    display: flex;
+    display: none;
     flex-direction: column;
     justify-content: space-evenly;
     height: 9.6rem;
